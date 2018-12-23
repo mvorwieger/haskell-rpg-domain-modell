e749a801653555afb5db37ba2fa4c1b0e1ce7d09
mvorwieger <michel.vorwieger@outlook.de>
2018-12-23 22:51:05 +0100

    refactored

--- src/Item.hs
+++ src/Item.hs
@@ -1,20 +1,14 @@
-{-# LANGUAGE TemplateHaskell #-}
-module Item (Item, BaseItem) where
+module Item (Item(..), BaseItem(..)) where
 
-import Control.Lens
 import Money 
 
-data BaseItem = BaseItem { _name :: String
+data BaseItem = BaseItem { _itemName :: String
                          , _price :: Money
                          } deriving (Show, Eq)
 
 data Item = OffenceItem { _base :: BaseItem, _damage :: Int }
           | DefenceItem { _base :: BaseItem, _armour :: Int }
+          | UselessItem { _base :: BaseItem }
           deriving (Show, Eq)
-
-makeLenses ''BaseItem
-makeLenses ''Item
-
-
 
 
--- src/Unit.hs
+++ src/Unit.hs
@@ -12,14 +12,16 @@ import Item
 data Unit = Unit { _health :: Int
                  , _attackDamage :: Int 
                  } deriving (Show, Eq)
-makeLenses ''Unit
 
 data Player = Player { _name        :: String
                      , _stats       :: Unit
                      , _money       :: Money
                      , _inventory   :: [Item]
                      } deriving (Show, Eq)
+makeLenses ''Unit
 makeLenses ''Player
+makeLenses ''BaseItem
+makeLenses ''Item
 
 attack :: Int -> Unit -> Unit
 attack damageTaken = health -~ damageTaken 
@@ -37,6 +39,9 @@ emptyWallet = Money { unMoney = 0 }
 
 u1 = Unit {_attackDamage = 10, _health = 200}
 u2 = Unit {_attackDamage = 20, _health = 100}
+i1 = UselessItem { _base = BaseItem { _itemName = "Test", _price = 0 }}
 
-p1 = Player { _name = "Michel", _stats = u1, _money = emptyWallet, _inventory = []}
+p1 = Player { _name = "Michel", _stats = u1, _money = emptyWallet, _inventory = [i1]}
 p2 = Player { _name = "Robin", _stats = u2, _money = emptyWallet, _inventory = []}
+
+
